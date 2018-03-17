0fdb4d8a2b0118416b5cbd13fc90eef62375ee70
Flávio <flaviolive3@hotmail.com>
2018-03-17 14:40:46 -0300

    Que bacana!! Agora temos o sobre, ou pelo menos quase...
    Adicionado a chamada da tela sobre na aplicação. Criação de metodo para construção de janelas, refatoração.

--- app/view/principal/js/index-render.js
+++ app/view/principal/js/index-render.js
@@ -1,1 +1,13 @@
-console.log('Estou na janela do navegador meu caro!');
+//importando o IPC responsável pela comunicação entre o processo principal e o de render
+const {ipcRenderer} = require('electron'); //ipcRender comunicação do processo de render
+
+let linkSobre = document.querySelector("#link-sobre");
+
+//#region eventos
+
+linkSobre.addEventListener('click',function(){
+    //envia um envento para o processo principal
+    ipcRenderer.send("abrir-tela-sobre");
+});
+
+//#endregion
--- main.js
+++ main.js
@@ -1,46 +1,106 @@
 //importando o submodulo app do electron utiliza-se as {} para indicar que estamos pegando apenas o submodulo app do electron
 //importando o modulo responsavel por criar as janelas
-const { app, BrowserWindow } = require('electron');
+//importando o modulo responsavel por escutar os eventos do render
+const { app, BrowserWindow, ipcMain } = require('electron');
 
 //file:// -> file protocol
 let _diretorio = {
-    app : `file://${__dirname}/app/`,
-    view:`file://${__dirname}/app/view/`
-}; 
+    app: `file://${__dirname}/app/`,
+    view: `file://${__dirname}/app/view/`
+};
 
 var rotasView = {
-    principal: _diretorio.view + `principal/index.html`
-}
+    principal: _diretorio.view + `principal/index.html`,
+    sobre: _diretorio.view + `sobre/sobre.html`
+};
+
+var telasAbertas = [];
 
 
-//#region eventos
+//#region eventos app
 
 //quando a aplicação for inicializada faça açgo
-app.on('ready',start);
+app.on('ready', start);
 
 //quando todas janelas fecharem
-app.on('window-all-closed',exit);
+app.on('window-all-closed', exit);
 
+//#endregion
+
+//#region eventos ipcMain
+ipcMain.on("abrir-tela-sobre", abrirTelaSobre);
 //#endregion
 
 //#region metodos
 
 //função para inicialização do sistema
 function start() {
-   console.log('Aplicação iniciada...');
-   //criando janela principal
-   let janelaPrincipal = new BrowserWindow({
-       width: 600, //largura
-       heigth: 400 //altura
-    });
+    console.log('Aplicação iniciada...');
 
-    //carregando html para a janela __dirname => diretório local
-    janelaPrincipal.loadURL(rotasView.principal);
+    //metodo responsavel por construir janelas
+    let configPrincipal = {
+        id: 'principal',
+        width: 600,
+        heigth: 300
+    };
+    _constroiJanela(configPrincipal, rotasView.principal);
+
 };
 
 //fecha a aplicação
-function exit(){
+function exit() {
     console.log('Aplicação encerrada...');
     app.quit();
 };
+
+function abrirTelaSobre() {
+    let configSobre = {
+        id: 'sobre',
+        width: 300,
+        heigth: 100
+    };
+    return _constroiJanela(configSobre, rotasView.sobre);
+}
+//#endregion
+
+//#region metodosInternos
+function _constroiJanela(config, url) {
+    if (_TelaEstaAberta(config.id))
+        return;
+
+    var browserJanela = new BrowserWindow(config);
+    //criando janela 
+    let janela = browserJanela;
+    //carregando html para a janela
+    janela.loadURL(url);
+    //adiciona o identificador da tela no array
+    telasAbertas.push(config.id);
+    //escuta o evento de closed da janela
+    //adicionando evento na janela
+    janela.on('closed', () => {
+        _removeIdjanelaTelasAbertas(config.id);
+    });
+}
+
+//percorre o array com o id das telas e verifica se ela esta aberta
+function _TelaEstaAberta(id) {
+
+    if (telasAbertas != null) {
+        for (i = 0; i < telasAbertas.length; i++) {
+            if (telasAbertas[i].trim() == id.trim())
+                return true;
+        }
+        return false;
+    }
+    return false;
+
+
+
+}
+
+function _removeIdjanelaTelasAbertas(id) {
+    //remove o item do array
+    telasAbertas =  telasAbertas.filter(idTela => idTela.trim() !== id.trim())
+}
+
 //#endregion
